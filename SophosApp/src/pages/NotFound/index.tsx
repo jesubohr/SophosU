5ac22177a9819bde90ce0f13254a5de8c4a410ee
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-10-02 16:43:59 -0500

    [core] Basic NotFound Page

---
+++ SophosApp/src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+import styles from "./styles.module.css"
+
+export const NotFound = () => {
+  return (
+    <main className={ styles.container }>
+      <h1 className={ styles.title }>404</h1>
+      <p className={ styles.description }>
+        Sorry, the page you were looking for doesn't exist.
+        <br />
+        Maybe you want to go to the <Link to="/">home page</Link>?
+      </p>
+    </main>
+  )
+}
