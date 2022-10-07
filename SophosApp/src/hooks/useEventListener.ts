d29d6c65107efeb479d371ffbf79f2e8492efa64
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-10-07 02:19:39 -0500

    [core] Hook that replaces use of media-query

---
+++ SophosApp/src/hooks/useEventListener.ts
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from "react"
+
+export function useEventListener (type: string, callback: (event: Event) => void, element: HTMLElement | Window = window) {
+  const callbackRef = useRef(callback)
+
+  useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    const handler = (event: Event) => callbackRef.current(event)
+    element.addEventListener(type, handler)
+
+    return () => element.removeEventListener(type, handler)
+  }, [type, element])
+}
---
+++ SophosApp/src/hooks/useWindowResize.ts
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import { useEventListener } from "./useEventListener"
+
+export function useWindowSize () {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  })
+
+  useEventListener('resize', () => {
+    setWindowSize({ width: window.innerWidth, height: window.innerHeight })
+  })
+
+  return windowSize
+}
