2870dcb4d3965fee3a5379f9bfaf1e2bb2edb850
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-10-13 02:34:26 -0500

    [core] Create Register page

---
+++ SophosApp/src/pages/Register/RegisterView.tsx
@@ -0,0 +1,126 @@
+import type { Intent } from "@blueprintjs/core"
+import { useState } from "react"
+import { FormGroup, InputGroup, Button } from "@blueprintjs/core"
+import { generatePassword, isSecurePassword } from "@/utils/authHelper"
+import styles from "./styles.module.css"
+
+export interface IRegisterViewProps {
+  emailHelperText: string
+  emailIntent: Intent
+  emailInputRef: React.RefObject<HTMLInputElement>
+  handleEmailInput: (event: React.FormEvent<HTMLInputElement>) => void
+
+  passwordHelperText: string
+  passwordIntent: Intent
+  passwordInputRef: React.RefObject<HTMLInputElement>
+  handlePasswordInput: (event: React.FormEvent<HTMLInputElement>) => void
+
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+  setIsSuccessPassword: React.Dispatch<React.SetStateAction<boolean | null>>
+}
+export const RegisterView = (props: IRegisterViewProps) => {
+  const { emailHelperText, emailIntent, emailInputRef, handleEmailInput } = props
+  const { passwordHelperText, passwordIntent, passwordInputRef, handlePasswordInput } = props
+  const { handleSubmit, setIsSuccessPassword } = props
+
+  return (
+    <main className={ styles.container }>
+      <h1 className={ "bp4-heading" + styles.title }>Register</h1>
+      <form onSubmit={ handleSubmit } className={ styles.form }>
+        <FormGroup
+          label="Email"
+          labelFor="email-input"
+          labelInfo="(required)"
+          subLabel={ emailHelperText }
+          intent={ emailIntent }
+        >
+          <InputGroup
+            inputRef={ emailInputRef }
+            id="email-input"
+            name="email"
+            large={ true }
+            intent={ emailIntent }
+            placeholder="user@email.com"
+            onInput={ handleEmailInput }
+            required
+          />
+        </FormGroup>
+        <FormGroup
+          helperText={ (
+            <p className={ styles.helperText }>
+              Password must be 12 characters long and contain uppercase letters, lowercase letters, numbers, and symbols.
+            </p>
+          ) }
+          label="Password"
+          labelFor="password-input"
+          labelInfo="(required)"
+          subLabel={ passwordHelperText }
+          intent={ passwordIntent }
+        >
+          <InputGroup
+            inputRef={ passwordInputRef }
+            id="password-input"
+            name="password"
+            type="password"
+            large={ true }
+            intent={ passwordIntent }
+            placeholder="$4|uT0n_M@ndo"
+            onInput={ handlePasswordInput }
+            rightElement={ (
+              <>
+                <CreatePassword inputRef={ passwordInputRef } successPassword={ setIsSuccessPassword } />
+                <ViewPassword inputRef={ passwordInputRef } />
+              </>
+            ) }
+            required
+          />
+        </FormGroup>
+        <Button
+          type="submit"
+          fill={ true }
+          large={ true }
+          intent="primary"
+          text="Register"
+        />
+      </form>
+    </main>
+  )
+}
+
+interface GeneratePasswordProps {
+  inputRef: React.RefObject<HTMLInputElement>
+  successPassword: React.Dispatch<React.SetStateAction<boolean | null>>
+}
+const CreatePassword = ({ inputRef, successPassword }: GeneratePasswordProps) => {
+  return (
+    <Button
+      icon="refresh"
+      minimal
+      onClick={ () => {
+        const password = generatePassword()
+        const passwordInput = inputRef.current as HTMLInputElement
+        passwordInput.value = password
+        successPassword(isSecurePassword(password))
+      } }
+    />
+  )
+}
+
+interface ViewPasswordProps {
+  inputRef: React.RefObject<HTMLInputElement>
+}
+const ViewPassword = ({ inputRef }: ViewPasswordProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <Button
+      icon={ isOpen ? "eye-off" : "eye-open" }
+      minimal
+      onClick={ () => {
+        const passwordInput = inputRef.current as HTMLInputElement
+        passwordInput.type = passwordInput.type === "password" ? "text" : "password"
+        setIsOpen(!isOpen)
+      } }
+    />
+  )
+}
---
+++ SophosApp/src/pages/Register/index.tsx
@@ -0,0 +1,110 @@
+import type { Intent } from "@blueprintjs/core"
+import { useState, useEffect, useRef } from "react"
+import { isValidEmail, isSecurePassword } from "@/utils/authHelper"
+import { useAuth } from "@/context/AuthContext"
+import { RegisterView } from "./RegisterView"
+
+export const Register = () => {
+  const { onRegister } = useAuth()
+
+  const emailInputRef = useRef<HTMLInputElement>(null)
+  const passwordInputRef = useRef<HTMLInputElement>(null)
+
+  const [emailHelperText, setEmailHelperText] = useState<string>("")
+  const [passwordHelperText, setPasswordHelperText] = useState<string>("")
+  const [isSuccessPassword, setIsSuccessPassword] = useState<boolean|null>(null)
+
+  const [emailIntent, setEmailIntent] = useState<Intent>("none")
+  const [passwordIntent, setPasswordIntent] = useState<Intent>("none")
+
+
+  function handleEmailInput (event: React.FormEvent<HTMLInputElement>) {
+    const email = event.currentTarget.value
+    if (isValidEmail(email)) {
+      setEmailHelperText("Valid Email")
+      setEmailIntent("success")
+    }
+    else {
+      if (email.length === 0) {
+        setEmailHelperText("")
+        setEmailIntent("none")
+        return
+      }
+      setEmailHelperText("Email is invalid")
+      setEmailIntent("danger")
+    }
+  }
+  function handlePasswordInput (event: React.FormEvent<HTMLInputElement>) {
+    const password = event.currentTarget.value
+    if (password.length === 0) return setIsSuccessPassword(null)
+
+    if (isSecurePassword(password)) setIsSuccessPassword(true)
+    else setIsSuccessPassword(false)
+  }
+  function handleSubmit (event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    const email = formData.get('email') as string
+    const password = formData.get('password') as string
+
+    if (email.length === 0) {
+      setEmailHelperText("Email is required")
+      setEmailIntent("danger")
+      emailInputRef.current?.focus()
+      return
+    }
+
+    if (password.length === 0) {
+      setPasswordHelperText("Password is required")
+      setPasswordIntent("danger")
+      passwordInputRef.current?.focus()
+      return
+    }
+
+    if (isValidEmail(email) && isSecurePassword(password)) {
+      onRegister({ email, password })
+      setEmailHelperText("")
+      setPasswordHelperText("")
+      setEmailIntent("none")
+      setPasswordIntent("none")
+      setTimeout(() => { form.reset() }, 0)
+    }
+  }
+
+  useEffect(() => {
+    emailInputRef.current?.focus()
+  }, [])
+
+  useEffect(() => {
+    if (isSuccessPassword === null) {
+      setPasswordIntent("none")
+      setPasswordHelperText("")
+      return
+    }
+    if (isSuccessPassword) {
+      setPasswordIntent("success")
+      setPasswordHelperText("Secure Password")
+    } else {
+      setPasswordIntent("danger")
+      setPasswordHelperText("Password is not secure")
+    }
+  }, [isSuccessPassword])
+
+  return (
+    <RegisterView
+      emailHelperText={ emailHelperText }
+      emailIntent={ emailIntent }
+      emailInputRef={ emailInputRef }
+      handleEmailInput={ handleEmailInput }
+
+      passwordHelperText={ passwordHelperText }
+      passwordIntent={ passwordIntent }
+      passwordInputRef={ passwordInputRef }
+      handlePasswordInput={ handlePasswordInput }
+
+      handleSubmit={ handleSubmit }
+      setIsSuccessPassword={ setIsSuccessPassword }
+    />
+  )
+}
