ed7f2ae3e3b89f6911fbad564d940a2e7ba14d04
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-10-18 00:13:57 -0500

    [core] Methods and data for mock fetching

--- SophosApp/src/api/auth.ts
+++ SophosApp/src/api/auth.ts
@@ -1,26 +1,169 @@
 import type { UserProp } from '@/types/AuthContext'
+const API_URL = (route: string) => `http://localhost:3000/api/${route}`
 
-const mockFetch = (url: string, options: any) => {
+function mockFetch<T> (url: string, options: any = {}) {
+  const wait = (ms: number) => new Promise(rs => setTimeout(rs, ms))
   return Promise.resolve({
     ok: true,
-    json: () => Promise.resolve({ token: 'averysecuretoken' })
+    json: async (data: any) => {
+      await wait(1000)
+      return data as T
+    },
   })
 }
 
-const API_URL = (route: string) => `http://localhost:3000/api/${route}`
-
+// Authentication
 export async function userLogin (user: UserProp) {
   const res = await mockFetch(API_URL('login'), {
     method: 'POST',
     body: JSON.stringify(user)
   })
-  return res.json()
+  return res.json({ token: 'averysecuretoken' })
 }
-
 export async function userRegister (user: UserProp) {
   const res = await mockFetch(API_URL('register'), {
     method: 'POST',
     body: JSON.stringify(user)
   })
-  return res.json()
+  return res.json({ token: 'averysecuretoken' })
+}
+export async function userLogout () {
+  const res = await mockFetch(API_URL('logout'), {
+    method: 'POST'
+  })
+  return res.json({})
+}
+
+// Records CRUD
+import { StudentsExample } from "@/examples/Students"
+import type { Student } from "@/types/RecordModel"
+export async function getStudents () {
+  const res = await mockFetch<Student[]>(API_URL('students'))
+  if (!res.ok) throw new Error('Error fetching students')
+  return res.json(StudentsExample)
+}
+export async function getStudent (code: string) {
+  const res = await mockFetch<Student>(API_URL(`students/${code}`))
+  if (!res.ok) throw new Error('Error fetching student')
+  return res.json(StudentsExample.find(s => s.code === code))
+}
+export async function createStudent (student: Student) {
+  const res = await mockFetch<Student>(API_URL('students'), {
+    method: 'POST',
+    body: JSON.stringify(student)
+  })
+  if (!res.ok) throw new Error('Error creating student')
+
+  StudentsExample.push({ ...student, id: StudentsExample.length + 1 })
+  return res.json(student)
+}
+export async function updateStudent (student: Student) {
+  const res = await mockFetch<Student>(API_URL(`students/${student.code}`), {
+    method: 'PUT',
+    body: JSON.stringify(student)
+  })
+  if (!res.ok) throw new Error('Error updating student')
+
+  const index = StudentsExample.findIndex(s => s.code === student.code)
+  StudentsExample[index] = { ...student, id: StudentsExample[index].id }
+  return res.json(student)
+}
+export async function deleteStudent (code: string) {
+  const res = await mockFetch<Student>(API_URL(`students/${code}`), {
+    method: 'DELETE'
+  })
+  if (!res.ok) throw new Error('Error deleting student')
+
+  StudentsExample.splice(StudentsExample.findIndex(s => s.code === code), 1)
+  return res.json({})
+}
+
+import { TeachersExample } from "@/examples/Teachers"
+import type { Teacher } from "@/types/RecordModel"
+export async function getTeachers () {
+  const res = await mockFetch<Teacher[]>(API_URL('teachers'))
+  if (!res.ok) throw new Error('Error fetching teachers')
+  return res.json(TeachersExample)
+}
+export async function getTeacher (code: string) {
+  const res = await mockFetch<Teacher>(API_URL(`teachers/${code}`))
+  if (!res.ok) throw new Error('Error fetching teacher')
+  return res.json(TeachersExample.find(s => s.code === code))
+}
+export async function createTeacher (teacher: Teacher) {
+  const res = await mockFetch<Teacher>(API_URL('teachers'), {
+    method: 'POST',
+    body: JSON.stringify(teacher)
+  })
+  if (!res.ok) throw new Error('Error creating teacher')
+
+  TeachersExample.push({ ...teacher, id: TeachersExample.length + 1 })
+  return res.json(teacher)
+}
+export async function updateTeacher (teacher: Teacher) {
+  const res = await mockFetch<Teacher>(API_URL(`teachers/${teacher.code}`), {
+    method: 'PUT',
+    body: JSON.stringify(teacher)
+  })
+  if (!res.ok) throw new Error('Error updating teacher')
+
+  const index = TeachersExample.findIndex(s => s.code === teacher.code)
+  TeachersExample[index] = { ...teacher, id: TeachersExample[index].id }
+  return res.json(teacher)
+}
+export async function deleteTeacher (code: string) {
+  const res = await mockFetch<Teacher>(API_URL(`teachers/${code}`), {
+    method: 'DELETE'
+  })
+  if (!res.ok) throw new Error('Error deleting teacher')
+
+  TeachersExample.splice(TeachersExample.findIndex(s => s.code === code), 1)
+  return res.json({})
+}
+
+import { CoursesExample } from "@/examples/Courses"
+import type { Course } from "@/types/RecordModel"
+export async function getCourses () {
+  const res = await mockFetch<Course[]>(API_URL('courses'))
+  if (!res.ok) throw new Error('Error fetching courses')
+  return res.json(CoursesExample)
+}
+export async function getCourse (code: string) {
+  const res = await mockFetch<Course>(API_URL(`courses/${code}`))
+  if (!res.ok) throw new Error('Error fetching course')
+  return res.json(CoursesExample.find(s => s.code === code))
+}
+export async function createCourse (course: Course) {
+  const res = await mockFetch<Course>(API_URL('courses'), {
+    method: 'POST',
+    body: JSON.stringify(course)
+  })
+  if (!res.ok) throw new Error('Error creating course')
+
+  CoursesExample.push({
+    ...course,
+    id: CoursesExample.length + 1,
+    available_quota: course.max_students
+  })
+  return res.json(course)
+}
+export async function updateCourse (course: Course) {
+  const res = await mockFetch<Course>(API_URL(`courses/${course.code}`), {
+    method: 'PUT',
+    body: JSON.stringify(course)
+  })
+  if (!res.ok) throw new Error('Error updating course')
+
+  const index = CoursesExample.findIndex(s => s.code === course.code)
+  CoursesExample[index] = { ...course, id: CoursesExample[index].id, available_quota: CoursesExample[index].available_quota }
+  return res.json(course)
+}
+export async function deleteCourse (code: string) {
+  const res = await mockFetch<Course>(API_URL(`courses/${code}`), {
+    method: 'DELETE'
+  })
+  if (!res.ok) throw new Error('Error deleting course')
+
+  CoursesExample.splice(CoursesExample.findIndex(s => s.code === code), 1)
+  return res.json({})
 }
---
+++ SophosApp/src/examples/Courses.ts
@@ -0,0 +1,101 @@
+export const CoursesExample = [{
+  "id": 1,
+  "code": "587-85-2277",
+  "name": "Training",
+  "precourse": "Services",
+  "faculty": "Human Resources",
+  "required_credits": 4,
+  "enrolled_students": 10,
+  "max_students": 21,
+  "available_quota": 0
+}, {
+  "id": 2,
+  "code": "123-45-5578",
+  "name": "Training",
+  "precourse": "Training",
+  "faculty": "Services",
+  "required_credits": 4,
+  "enrolled_students": 9,
+  "max_students": 16,
+  "available_quota": 10
+}, {
+  "id": 3,
+  "code": "345-85-3355",
+  "name": "Services",
+  "precourse": "Support",
+  "faculty": "Human Resources",
+  "required_credits": 5,
+  "enrolled_students": 13,
+  "max_students": 29,
+  "available_quota": 10
+}, {
+  "id": 4,
+  "code": "139-85-2299",
+  "name": "Human Resources",
+  "precourse": "Marketing",
+  "faculty": "Engineering",
+  "required_credits": 3,
+  "enrolled_students": 28,
+  "max_students": 27,
+  "available_quota": 0
+}, {
+  "id": 5,
+  "code": "890-85-9944",
+  "name": "Services",
+  "precourse": "Services",
+  "faculty": "Legal",
+  "required_credits": 5,
+  "enrolled_students": 33,
+  "max_students": 33,
+  "available_quota": 10
+}, {
+  "id": 6,
+  "code": "358-85-3300",
+  "name": "Research and Development",
+  "precourse": "Research and Development",
+  "faculty": "Accounting",
+  "required_credits": 5,
+  "enrolled_students": 14,
+  "max_students": 29,
+  "available_quota": 10
+}, {
+  "id": 7,
+  "code": "784-85-8811",
+  "name": "Business Development",
+  "precourse": "Services",
+  "faculty": "Human Resources",
+  "required_credits": 4,
+  "enrolled_students": 38,
+  "max_students": 14,
+  "available_quota": 10
+}, {
+  "id": 8,
+  "code": "503-85-5588",
+  "name": "Human Resources",
+  "precourse": "Accounting",
+  "faculty": "Engineering",
+  "required_credits": 4,
+  "enrolled_students": 32,
+  "max_students": 19,
+  "available_quota": 10
+}, {
+  "id": 9,
+  "code": "204-85-9300",
+  "name": "Business Development",
+  "precourse": "Product Management",
+  "faculty": "Support",
+  "required_credits": 5,
+  "enrolled_students": 33,
+  "max_students": 26,
+  "available_quota": 0
+}, {
+  "id": 10,
+  "code": "478-85-3359",
+  "name": "Training",
+  "precourse": "Human Resources",
+  "faculty": "Training",
+  "required_credits": 4,
+  "enrolled_students": 4,
+  "max_students": 40,
+  "available_quota": 10
+}]
