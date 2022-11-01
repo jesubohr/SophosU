0a543f72df86ae178794146e58ee909f16cf3645
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-11-01 03:11:28 -0500

    [core] Add PORT env variable for deploy

--- SophosApp/vite.config.ts
+++ SophosApp/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = parseInt(process.env.PORT) || 5173
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: { alias: { '@': '/src' } },
   plugins: [react()],
   server: {
-    host: true,
-    port: 5173
+    host: '0.0.0.0',
+    port: PORT
   }
 })
