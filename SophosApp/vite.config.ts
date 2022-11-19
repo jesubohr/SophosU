20268abb0d118ff39406b49754e7793e4e77c12e
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-11-19 02:55:20 -0500

    [core] Some changes

--- SophosApp/vite.config.ts
+++ SophosApp/vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const PORT = parseInt(process.env.PORT) || 5173
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: { alias: { '@': '/src' } },
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: PORT
-  }
+  plugins: [react()]
 })
