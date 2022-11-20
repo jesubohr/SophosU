47f994a1b2651213673f77ef7a2c87c9510bb24b
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-11-20 02:13:20 -0500

    [core] Add components for fetching side-effects

---
+++ SophosApp/src/components/Error/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.css"
+
+export interface IErrorProps {
+  message: string
+}
+
+export const ErrorView = (props: IErrorProps) => {
+  return (
+    <main className={ styles.container }>
+      <h1 className={ styles.title }>Error</h1>
+      <p className={ styles.description }>{ props.message }</p>
+    </main>
+  )
+}
---
+++ SophosApp/src/components/Loading/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css"
+
+export const LoadingView = () => {
+  return (
+    <div className={ styles.container }>
+      <div className={ styles.spinner }>
+        <div className={ styles.block }>
+          <div className={ styles.item }></div>
+          <div className={ styles.item }></div>
+          <div className={ styles.item }></div>
+          <div className={ styles.item }></div>
+          <div className={ styles.item }></div>
+          <div className={ styles.item }></div>
+          <div className={ styles.item }></div>
+          <div className={ styles.item }></div>
+        </div>
+      </div>
+    </div>
+  )
+}
