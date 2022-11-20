c8435551831d4a5b154e9b3a074f0295e6145a46
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-11-20 02:33:16 -0500

    [core] Add missing routes

--- SophosApp/src/main.tsx
+++ SophosApp/src/main.tsx
@@ -11,31 +11,59 @@ import { Login } from "@/pages/Login"
 import { Register } from "@/pages/Register"
 import { Home } from "@/pages/Home"
 
-import { ListStudents, AddStudent, EditStudent } from "@/pages/Students"
+import {
+  ListStudents,
+  AddStudent,
+  EditStudent,
+  ViewStudent
+} from "@/pages/Students"
+import {
+  ListTeachers,
+  AddTeacher,
+  EditTeacher,
+  ViewTeacher
+} from "@/pages/Teachers"
+import { ListCourses, AddCourse, EditCourse, ViewCourse } from "@/pages/Courses"
 
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={ new QueryClient() }>
+      <QueryClientProvider client={new QueryClient()}>
         <AuthProvider>
           <Routes>
-            <Route path="/" element={
-              <ProtectedRoute>
-                <App />
-              </ProtectedRoute>
-            }>
-              <Route index element={ <Home /> } />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <App />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Home />} />
               <Route path="/students">
-                <Route index element={ <ListStudents /> } />
-                <Route path="add" element={ <AddStudent /> } />
-                <Route path=":id/edit" element={ <EditStudent /> } />
+                <Route index element={<ListStudents />} />
+                <Route path="add" element={<AddStudent />} />
+                <Route path=":id" element={<ViewStudent />} />
+                <Route path="edit/:id" element={<EditStudent />} />
+              </Route>
+              <Route path="/teachers">
+                <Route index element={<ListTeachers />} />
+                <Route path="add" element={<AddTeacher />} />
+                <Route path=":id" element={<ViewTeacher />} />
+                <Route path="edit/:id" element={<EditTeacher />} />
+              </Route>
+              <Route path="/courses">
+                <Route index element={<ListCourses />} />
+                <Route path="add" element={<AddCourse />} />
+                <Route path=":id" element={<ViewCourse />} />
+                <Route path="edit/:id" element={<EditCourse />} />
               </Route>
             </Route>
-            <Route path="*" element={ <NotFound /> } />
-            <Route path="/login" element={ <Login /> } />
-            <Route path="/register" element={ <Register /> } />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </AuthProvider>
       </QueryClientProvider>
